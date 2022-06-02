a1a4ff7b169a290791a928ebcfac61dc155d29c2
thiagost26 <thiagost26@gmail.com>
2022-06-01 21:27:10 -0300

    funcionalidade do cadastro de dividendos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,15 +1,15 @@
-import './dashboard.css';
 import { useState } from 'react';
 
+import './dashboard.css';
 import Header from "../../components/Header";
 import Title from '../../components/Title';
 
-import { FcMoneyTransfer, FcPlus } from "react-icons/fc"
+import { FcMoneyTransfer, FcPlus, FcSearch, FcSupport } from "react-icons/fc"
 import { Link } from 'react-router-dom';
 
 
 export default function Dashboard() {
-    const [dividendos, setDividendos] = useState([]);
+    const [dividendos, setDividendos] = useState([1]);
 
 
     return(
@@ -31,10 +31,65 @@ export default function Dashboard() {
                     </div>                    
 
                 ) : (
-                    <div className="container dashboard">
-                        <span>Buscando dividendos...</span>
-                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Ativo</th>
+                                <th scope="col">Dividendo</th>
+                                <th scope="col">Qtd</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Data Pag</th>
+                                <th scope="col">Total</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr >
+                                <td data-label="Cliente">BBAS3</td>
+                                <td data-label="Dividendo">R$ 0,43</td>
+                                <td data-label="Qtd">200</td>
+                                <td data-label="Status">                                    
+                                    <span className="badge" style={{backgroundColor: '#5cb85c'}}>Aprovado</span>
+                                </td>
+                                <td data-label="Data">14/05/2022</td>
+                                <td data-label="Total">R$ 86,00</td>
+                                <td>
+                                    <button className="action" style={{backgroundColor: '#3583f6'}} >
+                                        <FcSearch color='#FFF' size={17} />
+                                    </button>
+                                    <button className="action" style={{backgroundColor: '#F6a935'}} >
+                                        <FcSupport color='#FFF' size={17} />
+                                    </button>
+                                </td>
+                            </tr>                                      
+                        </tbody>
+
+                        <tbody>
+                            <tr >
+                                <td data-label="Cliente">EGIE3</td>
+                                <td data-label="Dividendo">R$ 2,75</td>
+                                <td data-label="Qtd">500</td>
+                                <td data-label="Status">                                    
+                                    <span className="badge" style={{backgroundColor: '#5cb85c'}}>Aprovado</span>
+                                </td>
+                                <td data-label="Data">15/04/2022</td>
+                                <td data-label="Total">R$ 1.375,00</td>
+                                <td>
+                                    <button className="action" style={{backgroundColor: '#3583f6'}} >
+                                        <FcSearch color='#FFF' size={17} />
+                                    </button>
+                                    <button className="action" style={{backgroundColor: '#F6a935'}} >
+                                        <FcSupport color='#FFF' size={17} />
+                                    </button>
+                                </td>
+                            </tr>                                      
+                        </tbody>
+                    </table>
                 )}
+
+                <div>
+                    <label>Total geral: R$ 1.461,00</label>
+                </div>
 
 
 
--- src/pages/Dividendos/index.js
+++ src/pages/Dividendos/index.js
@@ -1,17 +1,103 @@
+import { useState, useEffect, useContext } from 'react';
 import './dividendos.css';
 
+import firebase from "../../services/firebaseConnection";
 import Header from '../../components/Header';
 import Title from '../../components/Title';
+import { AuthContext } from '../../contexts/auth';
 
 import { FcMoneyTransfer } from "react-icons/fc";
+import { toast } from "react-toastify";
 
 export default function Dividendos() {
 
+    const [loadAtivos, setLoadAtivos] = useState(true);
+    const [ativos, setAtivos] = useState([]);
+    const [ativoSelected, setAtivoSelected] = useState(0);
+
+    const [status, setStatus] = useState('Em aberto');
+    const [valor, setValor] = useState('');
+    const [qtd, setQtd] = useState('');
+    const [dataPagamento, setDataPagamento] = useState('');
+
+    const { user } = useContext(AuthContext);
 
 
-    function handleRegister(e) {
+    useEffect(() => {
+        async function loadAtivos() {
+            await firebase.firestore().collection('ativos')
+            .get()
+            .then((snapshot) => {
+                let lista = [];
+
+                snapshot.forEach((doc) => {
+                    lista.push({
+                        id: doc.id,
+                        ativo: doc.data().ativo
+                    })
+                })
+
+                if(lista.length === 0) {
+                    console.log('NENHUMA EMPRESA ENCONTRADA');
+                    setAtivos([ {id: 1, ativo: 'ATIVO'} ]);
+                    setLoadAtivos(false);
+                    return;
+                }
+
+                setAtivos(lista);
+                setLoadAtivos(false);
+
+            })
+            .catch((error) => {
+                console.log('DEU ALGUM ERRO', error);
+                setLoadAtivos(false);
+                setAtivos([ {id: 1, ativo: ''} ]);
+            })
+        }
+
+        loadAtivos();
+    }, [])
+
+
+
+    async function handleRegister(e) {
         e.preventDefault();
-        alert('lancado');
+
+        await firebase.firestore().collection('dividendos')
+        .add({
+            created: new Date(),
+            ativo: ativos[ativoSelected].ativo,
+            ativoId: ativos[ativoSelected].id,
+            status: status,            
+            valor: valor,
+            qtd: qtd,
+            dataPagamento: dataPagamento,
+            userId: user.uid
+        })
+        .then(() => {
+            toast.success('Chamado criado com sucesso!');
+            setQtd('');
+            setValor('');
+            setAtivoSelected(0);
+            setDataPagamento('');
+        })
+        .catch((error) => {
+            toast.error('Ops, erro ao registrar, tente mais tarde!');
+            console.log('Erro: ', error);
+        })
+    }
+
+
+    function handleOptionChange(e) {
+        setStatus(e.target.value);
+        console.log(e.target.value);
+    }
+
+
+    function handleChangeSelect(e) {
+        // console.log('INDEX DO CLIENTE SELECTIONADO: ', e.target.value);
+        // console.log('Cliente selecionado: ', ativos[e.target.value]);
+        setAtivoSelected(e.target.value);
     }
 
 
@@ -28,21 +114,54 @@ export default function Dividendos() {
                     <form className="form-profile" onSubmit={handleRegister}>
 
                         <label>Ativos</label>
-                        <select>
-                            <option value="">Selecione</option>
-                            <option value="BBAS3">BBAS3</option>
-                            <option value="EGIE3">EGIE3</option>
-                            <option value="MXRF11">MXRF11</option>
-                        </select>
+
+                        {loadAtivos ? (
+                            <input type="text" disabled={true} value="Carregando ativos..." />
+                        ) : (
+
+                            <select value={ativoSelected} onChange={handleChangeSelect}>
+                                {ativos.map((item, index) => {
+                                    return(
+                                        <option key={item.id} value={index}>
+                                            {item.ativo}
+                                        </option>
+                                    )
+                                })}
+                            </select>
+
+                        )}
+
 
                         <label>Quantidade</label>
-                        <input type="text" placeholder="Quantidade de ativos" />
+                        <input type="text" placeholder="Quantidade de ativos" value={qtd} onChange={(e) => setQtd(e.target.value)} />
 
                         <label>Data pagamento</label>
-                        <input type="date" name="data" />
+                        <input type="date" name="data" value={dataPagamento} onChange={(e) => setDataPagamento(e.target.value)} />
 
                         <label>Valor</label>
-                        <input type="text" placeholder="R$ valor" />
+                        <input type="text" placeholder="R$ valor" value={valor} onChange={(e) => setValor(e.target.value)} />
+                        
+
+                        <label>Status</label>
+                        <div className="status">
+                            <input 
+                            type="radio"
+                            name="radio"
+                            value="Aprovado"
+                            onChange={handleOptionChange}
+                            checked={status === 'Aprovado'}
+                            />
+                            <span>Aprovado</span>
+                            
+                            <input 
+                            type="radio"
+                            name="radio"
+                            value="Negado"
+                            onChange={handleOptionChange}
+                            checked={status === 'Negado'}
+                            />
+                            <span>Negado</span>
+                        </div>
 
                         <button type="submit">Lançar</button>
                     </form>
